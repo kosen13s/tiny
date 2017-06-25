3449f43dc1871844f57301ed488dfb14494600c9
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-06-25 17:27:04 +0900

    refs #5 Define `satisfy` parser.

--- src/TinyParser.hs
+++ src/TinyParser.hs
@@ -18,3 +18,20 @@ anyChar :: Parser
 anyChar (x:xs) = Right ([x], xs)
 anyChar _ = Left NotEnoughLength
 
+
+-- |
+-- generate a parser which parse an char that matches the predicate.
+--
+-- >>> let a2eParser = satisfy (`elem` ['a'..'e'])
+-- >>> runStateT a2eParser "ef"
+-- Right ("e","f")
+-- >>> runStateT a2eParser "fg"
+-- Left ConditionUnsatisfied
+--
+satisfy :: (Char -> Bool) -> Parser
+satisfy pred (x:xs)
+  | pred x = Right ([x], xs)
+  | otherwise = Left ConditionUnsatisfied
+
+satisfy _ _ = Left NotEnoughLength
+
