210b9b296925358cd3b763af540ae3cd9a3dcd21
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-06-25 17:24:42 +0900

    refs #5 Define data type `ParseFailed` and `Parser`.

--- src/TinyParser.hs
+++ src/TinyParser.hs
@@ -1,2 +1,10 @@
 module TinyParser where
 
+
+data ParseFailed =
+    NotEnoughLength |
+    ConditionUnsatisfied
+    deriving (Show, Eq)
+
+type Parser = String -> Either ParseFailed (String, String)
+
