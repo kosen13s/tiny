25785b63c00497443b3526006acbec66b0943b9e
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-06-25 22:28:41 +0900

    refs #5 Write four-arithmetical-operation test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Hspec
 import TinyParser
+import TokenParser
 import Control.Monad.State
 
 main :: IO ()
@@ -11,6 +12,16 @@ main = hspec $ do
             runStateT parse1plus1 "10+1" `shouldBe` Left ConditionUnsatisfied
         it "fails parsing \"5+\" because input too short." $
             runStateT parse1plus1 "5+" `shouldBe` Left NotEnoughLength
+
+    describe "four-arithmetical-operation" $ do
+        it "parses addition." $
+            evalStateT expression "-123+456" `shouldBe` Right "-123+456"
+        it "parses subtraction." $
+            evalStateT expression "0--0" `shouldBe` Right "0--0"
+        it "parses multiplication." $
+            evalStateT expression "2*12345678901234567890" `shouldBe` Right "2*12345678901234567890"
+        it "parses division." $
+            evalStateT expression "0/-0" `shouldBe` Right "0/-0"
 
 parse1plus1 :: Parser
 parse1plus1 = do
