34ad2f54090d9d83b80f8ecbae77fd07703bc964
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-06-25 18:48:18 +0900

    refs #5 Implement natural numbers parser.

--- src/TokenParser.hs
+++ src/TokenParser.hs
@@ -1,2 +1,22 @@
 module TokenParser where
 
+import TinyParser
+import Control.Monad.State
+
+
+-- |
+-- Parse /0|[1-9][0-9]*/.
+--
+-- >>> runStateT naturalNumber "0abc"
+-- Right ("0","abc")
+-- >>> runStateT naturalNumber "1024kib"
+-- Right ("1024","kib")
+-- >>> runStateT naturalNumber "N0 number"
+-- Left (ParseFailed [ConditionUnsatisfied,ConditionUnsatisfied])
+--
+naturalNumber :: Parser
+naturalNumber = char '0' <|> do
+    h <- nonZeroDigit
+    t <- closure digit
+    return (h ++ t)
+
