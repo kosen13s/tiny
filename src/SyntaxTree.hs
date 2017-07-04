a2a266d5ea8d331b7eb90768b0b77ff6febfe2fd
Roadagain <ringoh72@gmail.com>
2017-07-04 09:32:39 +0900

    refs #11 Construct syntax trees from tokens

---
+++ src/SyntaxTree.hs
@@ -0,0 +1,40 @@
+module SyntaxTree where
+
+
+import LexicalParser
+import Control.Monad.State
+
+
+data Tree a =
+    Empty |
+    Leaf a |
+    Unary a (Tree a) |
+    Binary (Tree a) a (Tree a)
+        deriving Show
+
+
+type LexicalTree = Tree String
+
+
+-- |
+-- modify a lexical tree from an array of tokens.
+-- >>> modifyTree (Unary "-" (Leaf "2")) [(Token BinaryOperator "+"), (Token Literal "1")]
+-- Binary (Unary "-" (Leaf "2")) "+" (Leaf "1")
+modifyTree :: LexicalTree -> [Token] -> LexicalTree
+modifyTree tree [] = tree
+modifyTree _ ((Token Literal v):xs) = modifyTree (Leaf v) xs
+modifyTree _ ((Token UnaryOperator op):x:xs) = modifyTree (Unary op (lexicalTree [x])) xs
+modifyTree left ((Token BinaryOperator op):xs) = Binary left op (lexicalTree xs)
+
+
+-- |
+-- generate a lexical tree from an array of tokens.
+-- >>> let p = lexicalTree <$> expression
+-- >>> evalStateT p "1+2"
+-- Right (Binary (Leaf "1") "+" (Leaf "2"))
+-- >>> evalStateT p "-1+2"
+-- Right (Binary (Unary "-" (Leaf "1")) "+" (Leaf "2"))
+-- >>> evalStateT p "1+-2"
+-- Right (Binary (Leaf "1") "+" (Unary "-" (Leaf "2")))
+lexicalTree :: [Token] -> LexicalTree
+lexicalTree = modifyTree Empty
